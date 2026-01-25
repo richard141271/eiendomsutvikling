cff5f923ef0f3beb974593f7b7ae90fed5596d55
Jørn`s Mackbook Pro <jornsmackbookpro@Jrns-sin-MacBook-Pro.local>
2026-01-25 22:41:16 +0100

    fix: add puppeteer-core and chromium to serverExternalPackages and cleanup package.json

--- next.config.js
+++ next.config.js
@@ -10,6 +10,9 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
+  },
 };
 
 module.exports = nextConfig;
